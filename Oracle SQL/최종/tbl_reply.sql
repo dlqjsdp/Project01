9b8ceb66e8a1bb6f8088724cf39cebcdbc64bd14
bbolung <shoh2802@gmail.com>
2025-05-24 14:10:41 +0900

    Add files via upload

--- Oracle SQL/최종/tbl_reply.sql
+++ Oracle SQL/최종/tbl_reply.sql
@@ -17,14 +17,6 @@ alter table tbl_reply add CONSTRAINT fk_reply FOREIGN key(bno) REFERENCES tbl_pe
 
 desc tbl_reply;
 
-select * from tbl_reply order by rno;
-
--- Test를 위한 댓글 추가
-insert into tbl_reply(rno, bno, reply, replyer)
-values(seq_reply.nextval, 2, '댓글 test 입력', '홍길동');
-
-commit;
-
 -- 댓글 확인
 select * 
 from tbl_reply
@@ -32,3 +24,27 @@ order by rno desc;
 
 -- 페이징 처리 index 생성
 create index idx_reply on tbl_reply (bno desc, rno asc);
+
+-- 댓글 Test 등록
+insert into tbl_reply (rno, bno, reply, replyer)
+values(seq_reply.nextval, 20, '얼른 가보고 싶어요.', 'replyer01');
+insert into tbl_reply (rno, bno, reply, replyer)
+values(seq_reply.nextval, 20, '포스터만 봐도 분위기 미쳤네요… 진짜 기대돼요.', 'replyer02');
+insert into tbl_reply (rno, bno, reply, replyer)
+values(seq_reply.nextval, 20, '드디어 열리는군요! 오랫동안 기다렸어요.', 'replyer03');
+insert into tbl_reply (rno, bno, reply, replyer)
+values(seq_reply.nextval, 20, '포스터 너무 예뻐요.', 'replyer04');
+insert into tbl_reply (rno, bno, reply, replyer)
+values(seq_reply.nextval, 20, '작년에 다녀왔는데 정말 좋았어요. 올해도 기대됩니다!', 'replyer05');
+insert into tbl_reply (rno, bno, reply, replyer)
+values(seq_reply.nextval, 20, '시간 맞춰서 친구랑 같이 가기로 했어요.', 'replyer06');
+insert into tbl_reply (rno, bno, reply, replyer)
+values(seq_reply.nextval, 20, '관람 후기 남겨주실 분 계신가요? 분위기 어떤지 궁금해요.', 'replyer07');
+insert into tbl_reply (rno, bno, reply, replyer)
+values(seq_reply.nextval, 20, '전시 주제가 너무 흥미롭네요. ', 'replyer08');
+insert into tbl_reply (rno, bno, reply, replyer)
+values(seq_reply.nextval, 20, '좋은 정보 감사합니다. 날짜 체크해둘게요!', 'replyer09');
+insert into tbl_reply (rno, bno, reply, replyer)
+values(seq_reply.nextval, 20, '이 전시회 정말 기대돼요! 꼭 가보고 싶네요.', 'replyer10');
+
+commit;
